d553d2c3c6cf0e752664bb71def7b693573fd670
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-05-22 20:37:28 +0300

    pre-build project'

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
 import './header/header';
 import './footer/footer';
+import './hero/hero';
+import './banner/banner';
+import './steps/steps';
+import './why/why';
+import './form/form';
+import './process/process';
