e84842abb206f1eb7dedd00279ac26206281194f
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-05-29 21:47:45 +0300

    adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-import '../components';
+import './../components/components';
+
+const burgerBtnHandler = () => {
+  let burgerMenu = document.getElementById('burgerMenu');
+  burgerMenu.classList.remove('header--burgerMenu-nonactive');
+  burgerMenu.classList.add('header--burgerMenu-active');
+};
