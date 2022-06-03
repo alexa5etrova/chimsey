7c1a5c1601f3450da3adecf1a3c1fdfed1efbd57
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-03 12:58:07 +0300

    routes fixed

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,1 @@
-
+[Ссылка на gh-pages](https://alexa5etrova.github.io/chimsey-pages/)
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,34 @@
 import './../components/components';
+const themeEl = document.getElementsByTagName('body')[0];
+if (localStorage.getItem('theme') == 'dark') {
+  themeEl.classList.remove('lightTheme');
+  themeEl.classList.add('darkTheme');
+} else {
+  themeEl.classList.add('lightTheme');
+  themeEl.classList.remove('darkTheme');
+}
+
+const themeChangeHandler = (event) => {
+  event.preventDefault();
+
+  if (themeEl.classList.contains('lightTheme')) {
+    themeEl.classList.remove('lightTheme');
+    themeEl.classList.add('darkTheme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    themeEl.classList.remove('darkTheme');
+    themeEl.classList.add('lightTheme');
+    localStorage.setItem('theme', 'light');
+  }
+};
 
 const burgerBtnHandler = () => {
   let burgerMenu = document.getElementById('burgerMenu');
-  burgerMenu.classList.remove('header--burgerMenu-nonactive');
-  burgerMenu.classList.add('header--burgerMenu-active');
+  if (burgerMenu.classList.contains('header--burgerMenu-nonactive')) {
+    burgerMenu.classList.remove('header--burgerMenu-nonactive');
+    burgerMenu.classList.add('header--burgerMenu-active');
+  } else {
+    burgerMenu.classList.remove('header--burgerMenu-active');
+    burgerMenu.classList.add('header--burgerMenu-nonactive');
+  }
 };
